f5e6fc097baeb6220098e333f5e2f22c32cea1e8
DeepainSaini <deepainsaini111@gmail.com>
2026-09-20 21:40:58 +0530

    Getting Started with Mongo DB

---
+++ util/database.js
@@ -0,0 +1,31 @@
+const mongodb = require('mongodb');
+const mongoClient = mongodb.MongoClient;
+
+let _db;
+
+const mongoConnect  = (callback) => {
+   
+  mongoClient.connect(
+  'mongodb+srv://Ajay:Ajay1234@cluster0.xghddon.mongodb.net/shop?appName=Cluster0'
+  )
+  .then((client)=>{
+    console.log("Connected!");
+    _db = client.db()
+    callback();
+  })
+  .catch((err)=>{
+    console.log(err);
+  })
+};
+
+const getDb = () => {
+
+  if(_db){
+    return _db;
+  }
+
+  throw "No database found!";
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
